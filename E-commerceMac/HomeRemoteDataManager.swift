af2b1bd93d72755b34e055190590b6e2b36f2f1f
MarioArceo <mario.arceo.r@hotmail.com>
2022-04-28 10:32:52 -0500

    Intento de Viper

---
+++ E-commerceMac/HomeRemoteDataManager.swift
@@ -0,0 +1,15 @@
+//
+//  HomeRemoteDataManager.swift
+//  E-commerceMac
+//
+//  Created by Mario Arceo on 28/04/22.
+//  
+//
+
+import Foundation
+
+class HomeRemoteDataManager:HomeRemoteDataManagerInputProtocol {
+    
+    var remoteRequestHandler: HomeRemoteDataManagerOutputProtocol?
+    
+}
